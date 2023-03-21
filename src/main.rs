74a31336e90c3c923594720355a80156168d2d94
pykrete <landysixten@gmail.com>
2023-03-21 22:53:15 +0100

    Fix websocket returning code 200

--- src/main.rs
+++ src/main.rs
@@ -78,8 +78,8 @@ async fn cpus(State(state): State<AppState>) -> impl IntoResponse {
 #[axum::debug_handler]
 async fn realtime_cpus(State(state): State<AppState>, ws: WebSocketUpgrade) -> impl IntoResponse {
     ws.on_upgrade(|ws: WebSocket| async {
-        realtime_cpus_stream(state, ws).await;
-    });
+        realtime_cpus_stream(state, ws).await
+    })
 }
 
 async fn realtime_cpus_stream(app_state: AppState, mut ws: WebSocket) {
